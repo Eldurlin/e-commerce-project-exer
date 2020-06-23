973508cc02451bda5dbc3314218969230e1b773f
AC <adam.cakala@gmail.com>
2020-06-23 13:10:18 +0200

    added search for products, defined routes, update ProductServive with a new method

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   listProducts() {
-    this.productService.getProductList().subscribe(
+    /**
+     * Check if id parameter is available.
+     */
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    /**
+     * Get the id param string. Convert to number by using "+" symbol.
+     * If not available - default to 1.
+     */
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    /**
+     * Get products dor the given category id.
+     */
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
